cbb6a22fa7f978292c1ccccf076c84e010014de5
Simon Ostermann <simono@coli.uni-saarland.de>
2019-03-27 14:40:29 +0100

    various changes to wording. added preview version of the website. added external pages for the shared task and linked data and submission instructions on codalab.

--- data/leaderboards/leaderboard1.js
+++ data/leaderboards/leaderboard1.js
@@ -1,1 +1,12 @@
 var tab = document.getElementById("task1-leaderboard");
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tab.appendChild(tr)
+td1.innerHTML = "1";
+td2.innerHTML = "ATTENTIVE READER BASELINE (single model)";
+td3.innerHTML = "0.692";
--- data/leaderboards/leaderboard2.js
+++ data/leaderboards/leaderboard2.js
@@ -1,1 +1,48 @@
 var tab = document.getElementById("task2-leaderboard");
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "1";
+td2.innerHTML = "Knowledge-enhanced BERT (single model) (Anonymous) ";
+td3.innerHTML = "73.01";
+
+td4.innerHTML = "74.76";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "2";
+td2.innerHTML = "DCReader+BERT (single model) (Anonymous)";
+td3.innerHTML = "70.49";
+
+td4.innerHTML = "71.98";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "3";
+td2.innerHTML = "BERT-Base (single model) (JHU [modification of the Google AI implementation]) https://arxiv.org/pdf/1810.04805.pdf";
+td3.innerHTML = "55.99";
+
+td4.innerHTML = "57.99";
