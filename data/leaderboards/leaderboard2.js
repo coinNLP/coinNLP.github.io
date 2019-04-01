653997115ce793e111027f61a762ae89703556d9
Simon Ostermann <simono@coli.uni-saarland.de>
2019-04-01 11:19:35 +0200

    final version of the website
    moved preview site to index.html. updated task2 leaderboard

--- data/leaderboards/leaderboard2.js
+++ data/leaderboards/leaderboard2.js
@@ -11,9 +11,9 @@ tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "1";
 td2.innerHTML = "Knowledge-enhanced BERT (single model) (Anonymous) ";
-td3.innerHTML = "73.01";
+td3.innerHTML = "71.6";
 
-td4.innerHTML = "74.76";
+td4.innerHTML = "73.62";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -27,9 +27,9 @@ tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "2";
 td2.innerHTML = "DCReader+BERT (single model) (Anonymous)";
-td3.innerHTML = "70.49";
+td3.innerHTML = "69.49";
 
-td4.innerHTML = "71.98";
+td4.innerHTML = "71.14";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -43,6 +43,6 @@ tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "3";
 td2.innerHTML = "BERT-Base (single model) (JHU [modification of the Google AI implementation]) https://arxiv.org/pdf/1810.04805.pdf";
-td3.innerHTML = "55.99";
+td3.innerHTML = "54.04";
 
-td4.innerHTML = "57.99";
+td4.innerHTML = "56.07";
