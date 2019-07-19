70f589fa67dbc8e08486668baed23975e66f444b
Sheng Zhang <zhangsheng.zero@gmail.com>
2019-07-19 09:59:05 -0400

    Remove KT-NET from Task 2.

--- data/leaderboards/leaderboard2.js
+++ data/leaderboards/leaderboard2.js
@@ -26,22 +26,6 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "2";
-td2.innerHTML = "KT-NET (single model) (Baidu NLP) ";
-td3.innerHTML = "71.60";
-
-td4.innerHTML = "73.62";
-
-var tr = document.createElement("tr");
-var td1 = document.createElement("td");
-var td2 = document.createElement("td");
-var td3 = document.createElement("td");
-var td4 = document.createElement("td");
-tr.appendChild(td1);
-tr.appendChild(td2);
-tr.appendChild(td3);
-tr.appendChild(td4);
-tab.appendChild(tr)
-td1.innerHTML = "3";
 td2.innerHTML = "SKG-BERT (single model) (Anonymous)";
 td3.innerHTML = "70.94";
 
@@ -57,7 +41,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "4";
+td1.innerHTML = "3";
 td2.innerHTML = "DCReader+BERT (single model) (Anonymous)";
 td3.innerHTML = "69.49";
 
@@ -73,7 +57,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "5";
+td1.innerHTML = "4";
 td2.innerHTML = "BERT-Base (single model) (JHU [modification of the Google AI implementation]) https://arxiv.org/pdf/1810.04805.pdf";
 td3.innerHTML = "54.04";
 
