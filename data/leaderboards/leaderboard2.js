e7335a9724d60622c9cd19a5ef5294a756e3d649
Sheng Zhang <zhangsheng.zero@gmail.com>
2019-07-22 21:11:34 -0700

    Update task2 results.

--- data/leaderboards/leaderboard2.js
+++ data/leaderboards/leaderboard2.js
@@ -10,6 +10,54 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "1";
+td2.innerHTML = "ENTLMR(single model) (PingAn Smart Health & SJTU)";
+td3.innerHTML = "82.72";
+
+td4.innerHTML = "83.38";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "2";
+td2.innerHTML = "CSRLM (single model) (PingAn Smart Health & SJTU)";
+td3.innerHTML = "81.48";
+
+td4.innerHTML = "82.24";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "3";
+td2.innerHTML = "ENTLM (single model) (PingAn Smart Health & SJTU)";
+td3.innerHTML = "80.64";
+
+td4.innerHTML = "82.10";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "4";
 td2.innerHTML = "LM_pretrained (single model) (paht)";
 td3.innerHTML = "76.18";
 
@@ -25,7 +73,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "2";
+td1.innerHTML = "5";
 td2.innerHTML = "SKG-BERT (single model) (Anonymous)";
 td3.innerHTML = "70.94";
 
@@ -41,7 +89,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "3";
+td1.innerHTML = "6";
 td2.innerHTML = "DCReader+BERT (single model) (Anonymous)";
 td3.innerHTML = "69.49";
 
@@ -57,7 +105,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "4";
+td1.innerHTML = "7";
 td2.innerHTML = "BERT-Base (single model) (JHU [modification of the Google AI implementation]) https://arxiv.org/pdf/1810.04805.pdf";
 td3.innerHTML = "54.04";
 
