7e3063dcb7b81b5a2e0afd3b465581dd074782c4
Sheng Zhang <zhangsheng.zero@gmail.com>
2019-07-25 20:14:23 -0700

    Push the test set leaderboards.

--- data/leaderboards/leaderboard2.js
+++ data/leaderboards/leaderboard2.js
@@ -10,10 +10,10 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "1";
-td2.innerHTML = "ENTLMR(single model) (PingAn Smart Health & SJTU)";
-td3.innerHTML = "82.72";
+td2.innerHTML = "ENTLM(ensemble) (PingAn Smart Health & SJTU)";
+td3.innerHTML = "83.09";
 
-td4.innerHTML = "83.38";
+td4.innerHTML = "83.74";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -26,10 +26,10 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "2";
-td2.innerHTML = "CSRLM (single model) (PingAn Smart Health & SJTU)";
-td3.innerHTML = "81.48";
+td2.innerHTML = "CSRLM (single model) (Anonymous)";
+td3.innerHTML = "81.78";
 
-td4.innerHTML = "82.24";
+td4.innerHTML = "82.58";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -43,9 +43,9 @@ tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "3";
 td2.innerHTML = "ENTLM (single model) (PingAn Smart Health & SJTU)";
-td3.innerHTML = "80.64";
+td3.innerHTML = "81.46";
 
-td4.innerHTML = "82.10";
+td4.innerHTML = "82.66";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -58,10 +58,10 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "4";
-td2.innerHTML = "LM_pretrained (single model) (paht)";
-td3.innerHTML = "76.18";
+td2.innerHTML = "{SKG-NET} (single model) (Anonymous)";
+td3.innerHTML = "79.66";
 
-td4.innerHTML = "77.68";
+td4.innerHTML = "81.11";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -74,10 +74,10 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "5";
-td2.innerHTML = "SKG-BERT (single model) (Anonymous)";
-td3.innerHTML = "70.94";
+td2.innerHTML = "{SKG-NET} (single model) (Anonymous)";
+td3.innerHTML = "79.48";
 
-td4.innerHTML = "71.55";
+td4.innerHTML = "80.04";
 
 var tr = document.createElement("tr");
 var td1 = document.createElement("td");
@@ -90,6 +90,38 @@ tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
 td1.innerHTML = "6";
+td2.innerHTML = "";
+td3.innerHTML = "76.79";
+
+td4.innerHTML = "78.16";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "7";
+td2.innerHTML = "SKG-BERT (single model) (Anonymous)";
+td3.innerHTML = "72.24";
+
+td4.innerHTML = "72.78";
+
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var td3 = document.createElement("td");
+var td4 = document.createElement("td");
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+tab.appendChild(tr)
+td1.innerHTML = "8";
 td2.innerHTML = "DCReader+BERT (single model) (Anonymous)";
 td3.innerHTML = "69.49";
 
@@ -105,7 +137,7 @@ tr.appendChild(td2);
 tr.appendChild(td3);
 tr.appendChild(td4);
 tab.appendChild(tr)
-td1.innerHTML = "7";
+td1.innerHTML = "9";
 td2.innerHTML = "BERT-Base (single model) (JHU [modification of the Google AI implementation]) https://arxiv.org/pdf/1810.04805.pdf";
 td3.innerHTML = "54.04";
 
